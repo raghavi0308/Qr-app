011952b9e556ab40f7c7d4e3e9a854dcf661c0e1
raghavi0308 <raghavijk3@gmail.com>
2025-06-16 03:59:30 +0530

    FInally

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/Qr-app/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
